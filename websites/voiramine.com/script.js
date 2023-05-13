f098157c300952ac562f1576052b62f53d9f5f3d
liamgen <74251382+liam-gen@users.noreply.github.com>
2023-05-13 15:54:50 +0200

    v1.0.3

---
+++ websites/voiramine.com/script.js
@@ -0,0 +1,6 @@
+/* Mitryguardians
+* Version : 1.0.0
+* GitHub : Mitryguardians
+*/
+// searchOne = extension.page.getElementByXpath("/html/body/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div/div[1]/div/div/div/div[1]/div/div[1]/div/div[3]/form/input[1]")
+// searchOne.className = "tryu"
